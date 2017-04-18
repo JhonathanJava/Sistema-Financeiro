f900c035df8e3eb70c138451426f41199ffe6875
Jhonathan <Jhonathanjava@gmail.com>
2017-04-17 21:20:42 -0300

    Sistema Financeiro Controllers

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Financeiro\Application;
 use Financeiro\ServiceContainer;
@@ -23,62 +22,6 @@ $app->get('/home/{name}/{id}', function(ServerRequestInterface $request){
 	return $response;
 });
 
-$app
-	->get('/category-costs',function() use ($app){
-		$view = $app->service('view.renderer');
-		$meuModel = new \Financeiro\Models\CentroCusto();
-		$categories = $meuModel->all();
-		
-		return $view->render('category-costs/list.html.twig', [
-			'categories' => $categories
-		]);
-	}, 'category-costs.list')
-
-	->get('/category-costs/new',function() use ($app){
-		$view = $app->service('view.renderer');
-		return $view->render('category-costs/create.html.twig');
-	}, 'category-costs.new')
-
-	->get('/category-costs/{id}/edit',function(ServerRequestInterface $request) use ($app){
-		$view = $app->service('view.renderer');
-		$id = $request->getAttribute('id');
-		$category = \Financeiro\Models\CentroCusto::findOrFail($id);
-		return $view->render('category-costs/edit.html.twig', [
-			'category' => $category]);
-	}, 'category-costs.edit')
-
-	->get('/category-costs/{id}/show',function(ServerRequestInterface $request) use ($app){
-		$view = $app->service('view.renderer');
-		$id = $request->getAttribute('id');
-		$category = \Financeiro\Models\CentroCusto::findOrFail($id);
-		return $view->render('category-costs/show.html.twig', [
-			'category' => $category]);
-	}, 'category-costs.show')
-
-	->get('/category-costs/{id}/delete',function(ServerRequestInterface $request) use ($app){
-		$id = $request->getAttribute('id');
-		$category = \Financeiro\Models\CentroCusto::findOrFail($id);
-		$category->delete();
-		return $app->route('category-costs.list');
-	}, 'category-costs.delete')
-	
-
-	->post('/category-costs/{id}/update',function(ServerRequestInterface $request) use ($app){
-		$id = $request->getAttribute('id');
-		$category = \Financeiro\Models\CentroCusto::findOrFail($id);
-		
-		$data = $request->getParsedBody();
-		$category->fill($data);
-		$category->save();
-		return $app->route('category-costs.list');
-	}, 'category-costs.update')
-
-
-	->post('/category-costs/store', function(ServerRequestInterface $request) use ($app) {
-		// cadastro de categoria
-		$data = $request->getParsedBody();
-		\Financeiro\Models\CentroCusto::create($data);
-		return $app->route('category-costs.list');
-	}, 'category-costs.store');
+require_once __DIR__ . '/../src/controllers/category-costs.php';
 
 $app->start();
